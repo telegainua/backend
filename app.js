1f6a76b41f60f708a88dc208a6ca3d91682e60ab
Roman Dubovyi <r.dubovoi@gmail.com>
2020-06-29 18:38:31 +0300

    Add cors

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 require("dotenv/config");
 
 //Middlwares
 app.use(bodyParser.json());
+app.use(cors());
 
 //Import routes
 const channelsRoute = require("./routers/channels");
